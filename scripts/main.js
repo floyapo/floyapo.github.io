7db09122afe4f4a5d0cb5d0f135f2acf97e47630
floyapo <44584053+floyapo@users.noreply.github.com>
2018-11-14 17:58:43 +0100

    chgt images

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 let monImage = document.querySelector('img');
 
 monImage.addEventListener('click', function() {
-	let monSrc = monImage.getAttribute('src');
-	if (monSrc === 'images/firefox.jpg') {
-		monImage.setAttribute('src', 'images/firefox2.jpg');	
-	} else {
-	  monImage.setAttribute('src', 'images/firefox.jpg');
-	}
+    let monSrc = monImage.getAttribute('src');
+    if (monSrc === 'images/firefox.jpg') {
+      monImage.setAttribute('src', 'images/firefox2.jpg');
+    } else {
+      monImage.setAttribute('src', 'images/firefox.jpg');
+    }
 });
