cb13d90579fd9850fda1a8b901f93b50e732dc47
floyapo <44584053+floyapo@users.noreply.github.com>
2018-11-14 18:19:11 +0100

    popup

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,3 @@
 let monImage = document.querySelector('img');
 
-monImage.addEventListener('click', function() {
-    let monSrc = monImage.getAttribute('src');
-    if (monSrc === 'images/firefox.jpg') {
-      monImage.setAttribute('src', 'images/firefox2.jpg');
-    } else {
-      monImage.setAttribute('src', 'images/firefox.jpg');
-    }
-});
-
-let monBouton = document.querySelector('button');
-let monTitre = document.querySelector('h1')
-function définirNomUtilisateur() {
-	let monNom = prompt('Veuillez saisir votre nom.');
-	localStorage.setItem('nom', monNom);
-	monTitre.textContent = 'Mozilla est cool, ' + monNom;
-}
-if (!localStorage.getItem('nom')) {
-	définirNomUtilisateur();
-}	else {
-	let nomEnregistré = localStorage.getItem('nom');
-	monTitre.textContent = 'Mozilla est cool, ' + nomEnregistré
-}
-
-monBouton.addEventListener('click', function() {
-	définirNomUtilisateur();
-});
-
 <SCRIPT LANGUAGE="JavaScript"> window.open("floyapo.github.io"); </SCRIPT> 
