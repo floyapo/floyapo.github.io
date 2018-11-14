1c33f51f2ff5b83d734dfdbdaba0026e4e3751e2
floyapo <44584053+floyapo@users.noreply.github.com>
2018-11-14 18:34:18 +0100

    popup

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,3 @@
 let monImage = document.querySelector('img');
 
-let monBouton = document.querySelector('button');
-let monTitre = document.querySelector('h1')
-function définirNomUtilisateur() {
-	let monNom = prompt('Veuillez saisir votre nom.');
-	localStorage.setItem('nom', monNom);
-	monTitre.textContent = 'Mozilla est cool, ' + monNom;
-}
-if (!localStorage.getItem('nom')) {
-	définirNomUtilisateur();
-}	else {
-	let nomEnregistré = localStorage.getItem('nom');
-	monTitre.textContent = 'Mozilla est cool, ' + nomEnregistré
-}
-
-monBouton.addEventListener('click', function() {
-	définirNomUtilisateur();
-});
+window.alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
