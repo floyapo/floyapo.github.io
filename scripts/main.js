c0e07f8e685b8f45868dc1ed9abdb7c60e813db5
floyapo <44584053+floyapo@users.noreply.github.com>
2018-11-14 20:50:58 +0100

    ajout popup

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
-let monImage = document.querySelector('img');
+var popup = function(){
+	// Boîte de dialogue OK-CANCEL
+	popup = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+if (popup == true) {
+	// Redirection lien
+window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+	}
+};
+setTimeout(popup, 10000)
 
-window.alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
